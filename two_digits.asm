6964b6fff6ad9612f2055ae5d1e9412944f7a65f
Aditya Bandaru <127210540+Adityabandaru18@users.noreply.github.com>
2024-10-17 10:28:05 +0530

    Create two_digits.asm

---
+++ two_digits.asm
@@ -0,0 +1,32 @@
+ORG 100h          
+
+mov dx, OFFSET msg      
+MOV AH, 09H             
+INT 21H
+
+
+MOV AL, 64        
+MOV AH, 00h          
+
+
+MOV BL, 10              
+DIV BL                  
+ADD AL, 30H             
+MOV DL, AL  
+MOV BH,AH               
+MOV AH, 02H             
+INT 21H                 
+
+
+MOV AL, BH              
+ADD AL, 30H             
+MOV DL, AL              
+MOV AH, 02H             
+INT 21H                 
+
+
+MOV AH, 4CH             
+INT 21H                 
+msg db 'The two-digit numbers are: $'   
+
+end
